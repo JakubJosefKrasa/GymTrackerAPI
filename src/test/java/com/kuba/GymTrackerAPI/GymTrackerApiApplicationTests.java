8bd2abf7df5ac1a4c55ec6d57742371f155ee346
JakubJosefKrasa <jjkrasa@gmail.com>
2024-06-25 01:15:19 +0200

    Initial commit

---
+++ src/test/java/com/kuba/GymTrackerAPI/GymTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuba.GymTrackerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
