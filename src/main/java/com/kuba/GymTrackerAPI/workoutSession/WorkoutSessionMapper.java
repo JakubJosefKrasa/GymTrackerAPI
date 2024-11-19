6b3e5a714cc7dfc9db693744e415bedde4246971
JakubJosefKrasa <jjkrasa@gmail.com>
2024-11-19 05:05:14 +0100

    Changed custom DTO Mappers to MapStruct mappers

---
+++ src/main/java/com/kuba/GymTrackerAPI/workoutSession/WorkoutSessionMapper.java
@@ -0,0 +1,20 @@
+package com.kuba.GymTrackerAPI.workoutSession;
+
+import com.kuba.GymTrackerAPI.trainingPlan.TrainingPlanWorkoutSessionExercisesDTO;
+import com.kuba.GymTrackerAPI.workoutSessionExercise.WorkoutSessionExerciseMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = WorkoutSessionExerciseMapper.class)
+public interface WorkoutSessionMapper {
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "date", source = "date")
+    @Mapping(target = "trainingPlan", source = "workoutSession")
+    WorkoutSessionDTO toWorkoutSessionDTO(WorkoutSession workoutSession);
+
+    @Mapping(target = "id", source = "trainingPlan.id")
+    @Mapping(target = "trainingPlanName", source = "trainingPlan.trainingPlanName")
+    @Mapping(target = "workoutSessionExercises", source = "workoutSessionExercises")
+    TrainingPlanWorkoutSessionExercisesDTO toTrainingPlanWorkoutSessionExercisesDTO(WorkoutSession workoutSession);
+}
---
+++ src/main/java/com/kuba/GymTrackerAPI/workoutSessionExerciseSet/SetMapper.java
@@ -0,0 +1,12 @@
+package com.kuba.GymTrackerAPI.workoutSessionExerciseSet;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface SetMapper {
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "repetitions", source = "repetitions")
+    @Mapping(target = "weight", source = "weight")
+    SetDTO toSetDTO(WorkoutSessionExerciseSet workoutSessionExerciseSet);
+}
