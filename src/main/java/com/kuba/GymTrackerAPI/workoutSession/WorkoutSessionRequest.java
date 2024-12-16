c23013441c078c81c3b07985f10de17adad55642
JakubJosefKrasa <jjkrasa@gmail.com>
2024-12-17 00:08:33 +0100

    Validations for WorkoutSessionRequest added

--- src/main/java/com/kuba/GymTrackerAPI/workoutSession/WorkoutSessionRequest.java
+++ src/main/java/com/kuba/GymTrackerAPI/workoutSession/WorkoutSessionRequest.java
@@ -1,9 +1,13 @@
 package com.kuba.GymTrackerAPI.workoutSession;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record WorkoutSessionRequest(
+        @NotNull(message = "Datum nesmí být prázdný!")
         LocalDate date,
+        @NotNull(message = "Tréninkový plán nesmí být prázdný!")
         Long trainingPlanId
 ) {
 }
