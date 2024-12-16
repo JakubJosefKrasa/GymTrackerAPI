768872efd7d7047662d0ceb51402afc70f198923
JakubJosefKrasa <jjkrasa@gmail.com>
2024-12-17 00:45:10 +0100

    Added logs for TrainingPlanService

--- src/main/java/com/kuba/GymTrackerAPI/trainingPlan/TrainingPlanService.java
+++ src/main/java/com/kuba/GymTrackerAPI/trainingPlan/TrainingPlanService.java
@@ -9,6 +9,7 @@ import com.kuba.GymTrackerAPI.security.UserContext;
 import com.kuba.GymTrackerAPI.user.User;
 import com.kuba.GymTrackerAPI.workoutSessionExercise.WorkoutSessionExercise;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +20,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TrainingPlanService {
@@ -28,11 +30,18 @@ public class TrainingPlanService {
     private final TrainingPlanMapper trainingPlanMapper;
 
     public TrainingPlan getTrainingPlanEntityById(Long id, User user) {
-        return trainingPlanRepository.findByIdAndUser(id, user).orElseThrow(() -> new NotFoundException("Tréninkový plán nenalezen!"));
+        log.info("[METHOD]: getTrainingPlanEntityById - Fetching trainingPlan by ID: {} and user_id: {}", id, user.getId());
+
+        return trainingPlanRepository.findByIdAndUser(id, user).orElseThrow(() -> {
+            log.warn("[METHOD]: getTrainingPlanEntityById - trainingPlan was not found by ID: {} and user_id: {}", id, user.getId());
+
+            return new NotFoundException("Tréninkový plán nenalezen!");
+        });
     }
 
     public PaginationDTO<TrainingPlanExercisesDTO> getTrainingPlansByUser(int pageNumber, int pageSize) {
         User user = userContext.getAuthenticatedUser();
+        log.info("[METHOD]: getTrainingPlansByUser - Fetching training plans for user_id: {} - pageNumber: {} - pageSize: {}", user.getId(), pageNumber, pageSize);
 
         Pageable page;
         if (pageNumber >= 0) {
@@ -59,6 +68,7 @@ public class TrainingPlanService {
 
     public TrainingPlanExercisesDTO getTrainingPlanById(Long id) {
         User user = userContext.getAuthenticatedUser();
+        log.info("[METHOD]: getTrainingPlanById - Fetching trainingPlan by ID: {} for user_id: {}", id, user.getId());
 
         TrainingPlan trainingPlan = getTrainingPlanEntityById(id, user);
 
@@ -68,6 +78,7 @@ public class TrainingPlanService {
     @Transactional
     public TrainingPlanExercisesDTO createTrainingPlan(TrainingPlanRequest trainingPlanRequest) {
         User user = userContext.getAuthenticatedUser();
+        log.info("[METHOD]: createTrainingPlan - Creating trainingPlan with trainingPlanRequest: {} for user_id: {}", trainingPlanRequest, user.getId());
 
         TrainingPlan trainingPlanToBeSaved = TrainingPlan.builder()
                 .trainingPlanName(trainingPlanRequest.trainingPlanName())
@@ -76,6 +87,7 @@ public class TrainingPlanService {
                 .build();
 
         trainingPlanToBeSaved = trainingPlanRepository.save(trainingPlanToBeSaved);
+        log.info("[METHOD]: createTrainingPlan - trainingPlan created with trainingPlanName: {} and ID: {}", trainingPlanToBeSaved.getTrainingPlanName(), trainingPlanToBeSaved.getId());
 
         return trainingPlanMapper.toTrainingPlanExercisesDTO(trainingPlanToBeSaved);
     }
@@ -83,18 +95,25 @@ public class TrainingPlanService {
     @Transactional
     public void deleteTrainingPlanById(Long id) {
         User user = userContext.getAuthenticatedUser();
+        log.info("[METHOD]: deleteTrainingPlanById - Deleting trainingPlan by ID: {} and user_id: {}", id, user.getId());
 
         TrainingPlan trainingPlan = getTrainingPlanEntityById(id, user);
+
         trainingPlanRepository.delete(trainingPlan);
+        log.info("[METHOD]: deleteTrainingPlanById - trainingPlan with ID: {} was deleted", id);
     }
 
     @Transactional
     public TrainingPlanExercisesDTO changeTrainingPlanName(Long id, TrainingPlanRequest trainingPlanRequest) {
         User user = userContext.getAuthenticatedUser();
+        log.info("[METHOD]: changeTrainingPlanName - Changing trainingPlanName of trainingPlan by ID: {} with trainingPlanRequest {} for user_id: {}", id, trainingPlanRequest, user.getId());
 
         TrainingPlan trainingPlan = getTrainingPlanEntityById(id, user);
+
         trainingPlan.setTrainingPlanName(trainingPlanRequest.trainingPlanName());
+
         trainingPlan = trainingPlanRepository.save(trainingPlan);
+        log.info("[METHOD]: changeTrainingPlanName - Changed trainingPlanName of trainingPlan by ID: {} to {}", id, trainingPlan.getTrainingPlanName());
 
         return trainingPlanMapper.toTrainingPlanExercisesDTO(trainingPlan);
     }
@@ -102,11 +121,16 @@ public class TrainingPlanService {
     @Transactional
     public TrainingPlanExercisesDTO addExerciseInTrainingPlan(Long trainingPlanId, Long exerciseId) {
         User user = userContext.getAuthenticatedUser();
+        log.info("[METHOD]: addExerciseInTrainingPlan - Adding exercise by ID: {} in trainingPlan by ID: {} for user_id: {}", exerciseId, trainingPlanId, user.getId());
 
         TrainingPlan trainingPlan = getTrainingPlanEntityById(trainingPlanId, user);
         Exercise exercise = exerciseService.getExerciseEntityById(exerciseId, user);
 
-        if (trainingPlan.getExercises().contains(exercise)) throw new BadRequestException("Cvik může být pouze jednou v tréninkovém plánu!");
+        if (trainingPlan.getExercises().contains(exercise)) {
+            log.error("[METHOD]: addExerciseInTrainingPlan - Exercise by ID: {} is already in trainingPlan by ID: {} for user_id: {}", exerciseId, trainingPlanId, user.getId());
+
+            throw new BadRequestException("Cvik může být pouze jednou v tréninkovém plánu!");
+        }
 
         trainingPlan.getExercises().add(exercise);
 
@@ -118,9 +142,14 @@ public class TrainingPlanService {
                             .workoutSessionExerciseSets(new ArrayList<>())
                             .build();
 
+            log.info(
+                    "[METHOD]: addExerciseInTrainingPlan - Creating workoutSessionExercise for exercise by ID: {} for workoutSession by ID {} in trainingPlan by ID: {} for user_id: {}",
+                    exerciseId, workoutSession.getId(), trainingPlanId, user.getId()
+            );
             workoutSession.getWorkoutSessionExercises().add(workoutSessionExercise);
         });
 
+        log.info("[METHOD]: addExerciseInTrainingPlan - Saving added exercise by ID: {} in trainingPlan by ID: {} for user_id: {}", exerciseId, trainingPlanId, user.getId());
         trainingPlan = trainingPlanRepository.save(trainingPlan);
 
         return trainingPlanMapper.toTrainingPlanExercisesDTO(trainingPlan);
@@ -129,6 +158,7 @@ public class TrainingPlanService {
     @Transactional
     public void removeExerciseFromTrainingPlan(Long trainingPlanId, Long exerciseId) {
         User user = userContext.getAuthenticatedUser();
+        log.info("[METHOD]: removeExerciseFromTrainingPlan - Removing exercise by ID: {} from trainingPlan by ID: {} for user_id: {}", exerciseId, trainingPlanId, user.getId());
 
         TrainingPlan trainingPlan = getTrainingPlanEntityById(trainingPlanId, user);
         Exercise exercise = exerciseService.getExerciseEntityById(exerciseId, user);
@@ -136,5 +166,6 @@ public class TrainingPlanService {
         trainingPlan.getExercises().remove(exercise);
 
         trainingPlanRepository.save(trainingPlan);
+        log.info("[METHOD]: removeExerciseFromTrainingPlan - Exercise by ID: {} was removed from trainingPlan by ID: {} for user_id: {}", exerciseId, trainingPlanId, user.getId());
     }
 }
