46d01e5fa328075f0407ab0132980713d1185307
JakubJosefKrasa <jjkrasa@gmail.com>
2024-12-17 00:45:57 +0100

    Added logs for WorkoutSessionExerciseService

--- src/main/java/com/kuba/GymTrackerAPI/workoutSessionExercise/WorkoutSessionExerciseService.java
+++ src/main/java/com/kuba/GymTrackerAPI/workoutSessionExercise/WorkoutSessionExerciseService.java
@@ -3,14 +3,22 @@ package com.kuba.GymTrackerAPI.workoutSessionExercise;
 import com.kuba.GymTrackerAPI.exceptions.NotFoundException;
 import com.kuba.GymTrackerAPI.workoutSession.WorkoutSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class WorkoutSessionExerciseService {
     private final WorkoutSessionExerciseRepository workoutSessionExerciseRepository;
 
     public WorkoutSessionExercise getWorkoutSessionExerciseEntityByIdAndWorkoutSession(Long workoutSessionExerciseId, WorkoutSession workoutSession) {
-        return workoutSessionExerciseRepository.findByIdAndWorkoutSession(workoutSessionExerciseId, workoutSession).orElseThrow(() -> new NotFoundException("Cvik v tréninku nenalezen!"));
+        log.info("[METHOD]: getWorkoutSessionExerciseEntityByIdAndWorkoutSession - Fetching workoutSessionExercise by ID: {} and workoutSession_id: {}", workoutSessionExerciseId, workoutSession.getId());
+
+        return workoutSessionExerciseRepository.findByIdAndWorkoutSession(workoutSessionExerciseId, workoutSession).orElseThrow(() -> {
+            log.warn("[METHOD]: getWorkoutSessionExerciseEntityByIdAndWorkoutSession - workoutSessionExercise was not found by ID: {} and workoutSession_id: {}", workoutSessionExerciseId, workoutSession.getId());
+
+            return new NotFoundException("Cvik v tréninku nenalezen!");
+        });
     }
 }
