b292b0c48688317abf826328a3c7c1d43cecf40d
JakubJosefKrasa <jjkrasa@gmail.com>
2024-12-17 00:46:16 +0100

    Added logs for WorkoutSessionExerciseSetService

--- src/main/java/com/kuba/GymTrackerAPI/workoutSessionExerciseSet/WorkoutSessionExerciseSetService.java
+++ src/main/java/com/kuba/GymTrackerAPI/workoutSessionExerciseSet/WorkoutSessionExerciseSetService.java
@@ -2,22 +2,35 @@ package com.kuba.GymTrackerAPI.workoutSessionExerciseSet;
 
 import com.kuba.GymTrackerAPI.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class WorkoutSessionExerciseSetService {
     private final WorkoutSessionExerciseSetRepository workoutSessionExerciseSetRepository;
 
     public WorkoutSessionExerciseSet getWorkoutSessionExerciseSetEntityById(Long workoutSessionExerciseSetId) {
-        return workoutSessionExerciseSetRepository.findById(workoutSessionExerciseSetId).orElseThrow(() -> new NotFoundException("Série nenalezena"));
+        log.info("[METHOD]: getWorkoutSessionExerciseSetEntityById - Fetching workoutSessionExerciseSet by ID: {}", workoutSessionExerciseSetId);
+
+        return workoutSessionExerciseSetRepository.findById(workoutSessionExerciseSetId).orElseThrow(() -> {
+            log.warn("[METHOD]: getWorkoutSessionExerciseSetEntityById - workoutSessionExerciseSet was not found by ID: {}", workoutSessionExerciseSetId);
+
+            return new NotFoundException("Série nenalezena");
+        });
     }
 
     public WorkoutSessionExerciseSet saveWorkoutSessionExerciseSet(WorkoutSessionExerciseSet workoutSessionExerciseSet) {
+        log.info("[METHOD]: saveWorkoutSessionExerciseSet - Saving workoutSessionExerciseSet");
+
         return workoutSessionExerciseSetRepository.save(workoutSessionExerciseSet);
     }
 
     public void deleteWorkoutSessionExerciseSetById(Long workoutSessionExerciseSetId) {
+        log.info("[METHOD]: deleteWorkoutSessionExerciseSetById - Deleting workoutSessionExerciseSet by ID: {}", workoutSessionExerciseSetId);
+
         workoutSessionExerciseSetRepository.deleteById(workoutSessionExerciseSetId);
+        log.info("[METHOD]: deleteWorkoutSessionExerciseSetById - workoutSessionExerciseSet by ID: {} was deleted", workoutSessionExerciseSetId);
     }
 }
