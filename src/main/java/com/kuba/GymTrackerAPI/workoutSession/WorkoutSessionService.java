dc54b0a4ea3a5b05f6a635d9ebecd637e0ba1994
JakubJosefKrasa <jjkrasa@gmail.com>
2024-12-17 00:45:39 +0100

    Added logs for WorkoutSessionService

--- src/main/java/com/kuba/GymTrackerAPI/workoutSession/WorkoutSessionService.java
+++ src/main/java/com/kuba/GymTrackerAPI/workoutSession/WorkoutSessionService.java
@@ -19,9 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class WorkoutSessionService {
     private final UserContext userContext;
     private final WorkoutSessionRepository workoutSessionRepository;
@@ -31,11 +31,18 @@ public class WorkoutSessionService {
     private final WorkoutSessionMapper workoutSessionMapper;
 
     public WorkoutSession getWorkoutSessionEntityById(Long workoutSessionId, User user) {
-        return workoutSessionRepository.findByIdAndUser(workoutSessionId, user).orElseThrow(() -> new NotFoundException("Trénink nenalezen!"));
+        log.info("[METHOD]: getWorkoutSessionEntityById - Fetching workoutSession by ID: {} and user_id: {}", workoutSessionId, user.getId());
+
+        return workoutSessionRepository.findByIdAndUser(workoutSessionId, user).orElseThrow(() -> {
+            log.warn("[METHOD]: getWorkoutSessionEntityById - workoutSession was not found by ID: {} and user_id: {}", workoutSessionId, user.getId());
+
+            return new NotFoundException("Trénink nenalezen!");
+        });
     }
 
     public List<WorkoutSessionDTO> getWorkoutSessionsByUser() {
         User user = userContext.getAuthenticatedUser();
+        log.info("[METHOD]: getWorkoutSessionsByUser - Fetching workout sessions for user_id: {}", user.getId());
 
         List<WorkoutSession> workoutSessions = workoutSessionRepository.findByUser(user);
 
@@ -47,6 +54,7 @@ public class WorkoutSessionService {
 
     public WorkoutSessionDTO getWorkoutSessionById(Long id) {
         User user = userContext.getAuthenticatedUser();
+        log.info("[METHOD]: getWorkoutSessionById - Fetching workoutSession by ID: {} for user_id: {}", id, user.getId());
 
         WorkoutSession workoutSession = getWorkoutSessionEntityById(id, user);
 
@@ -56,6 +64,10 @@ public class WorkoutSessionService {
     @Transactional
     public WorkoutSessionDTO createWorkoutSession(WorkoutSessionRequest workoutSessionRequest) {
         User user = userContext.getAuthenticatedUser();
+        log.info(
+                "[METHOD]: createWorkoutSession - Creating workout session with workoutSessionRequest: {} for user_id: {}",
+                workoutSessionRequest, user.getId()
+        );
 
         TrainingPlan trainingPlan = trainingPlanService.getTrainingPlanEntityById(workoutSessionRequest.trainingPlanId(), user);
 
@@ -73,11 +85,13 @@ public class WorkoutSessionService {
                     .workoutSessionExerciseSets(new ArrayList<>())
                     .build();
 
+            log.info("[METHOD]: createWorkoutSession - Creating workoutSessionExercise for exercise by ID: {} in workoutSession", exercise.getId());
             exercise.getWorkoutSessionExercises().add(workoutSessionExercise);
             workoutSession.getWorkoutSessionExercises().add(workoutSessionExercise);
         }
 
         workoutSession = workoutSessionRepository.save(workoutSession);
+        log.info("[METHOD]: createWorkoutSession - workoutSession created with ID: {}", workoutSession.getId());
 
         return workoutSessionMapper.toWorkoutSessionDTO(workoutSession);
     }
@@ -85,10 +99,12 @@ public class WorkoutSessionService {
     @Transactional
     public void deleteWorkoutSessionById(Long id) {
         User user = userContext.getAuthenticatedUser();
+        log.info("[METHOD]: deleteWorkoutSessionById - Deleting workoutSession by ID: {} and user_id: {}", id, user.getId());
 
         getWorkoutSessionEntityById(id, user);
 
         workoutSessionRepository.deleteById(id);
+        log.info("[METHOD]: deleteWorkoutSessionById - workoutSession by ID: {} was deleted", id);
     }
 
     @Transactional
@@ -98,12 +114,20 @@ public class WorkoutSessionService {
             WorkoutSessionExerciseSetRequest workoutSessionExerciseSetRequest
     ) {
         User user = userContext.getAuthenticatedUser();
+        log.info(
+                "[METHOD]: createExerciseSet - Creating exerciseSet for workoutSessionId: {} and workoutSessionExerciseId: {} with workoutSessionExerciseSetRequest: {}",
+                workoutSessionId, workoutSessionExerciseId, workoutSessionExerciseSetRequest
+        );
 
         WorkoutSession workoutSession = getWorkoutSessionEntityById(workoutSessionId, user);
 
         List<WorkoutSessionExercise> workoutSessionExercises = workoutSession.getWorkoutSessionExercises().stream().filter(wse -> wse.getId().equals(workoutSessionExerciseId)).toList();
 
-        if (workoutSessionExercises.isEmpty()) throw new NotFoundException("Cvik v tréninku nenalezen!");
+        if (workoutSessionExercises.isEmpty()) {
+            log.warn("[METHOD]: createExerciseSet - workoutSessionExercise by ID: {} wasn't found in workoutSession by ID: {}", workoutSessionExerciseId, workoutSessionId);
+
+            throw new NotFoundException("Cvik v tréninku nenalezen!");
+        }
 
         WorkoutSessionExerciseSet workoutExerciseSet = WorkoutSessionExerciseSet.builder()
                 .workoutSessionExercise(workoutSessionExercises.get(0))
@@ -112,8 +136,9 @@ public class WorkoutSessionService {
                 .build();
 
         workoutSessionExercises.get(0).getWorkoutSessionExerciseSets().add(workoutExerciseSet);
-        workoutSession = workoutSessionRepository.save(workoutSession);
 
+        log.info("[METHOD]: createExerciseSet - Saving workoutSessionExercise in workoutSession by ID: {}", workoutSessionId);
+        workoutSession = workoutSessionRepository.save(workoutSession);
 
         return workoutSessionMapper.toWorkoutSessionDTO(workoutSession);
     }
@@ -121,6 +146,10 @@ public class WorkoutSessionService {
     @Transactional
     public void deleteExerciseSetById(Long workoutSessionId, Long workoutSessionExerciseId, Long workoutSessionExerciseSetId) {
         User user = userContext.getAuthenticatedUser();
+        log.info(
+                "[METHOD]: deleteExerciseSetById - Deleting workoutSessionExerciseSet by ID: {} - workoutSessionExercise by ID: {}  and workoutSession by ID: {}",
+                workoutSessionExerciseSetId, workoutSessionExerciseId, workoutSessionId
+        );
 
         WorkoutSession workoutSession = getWorkoutSessionEntityById(workoutSessionId, user);
         workoutSessionExerciseService.getWorkoutSessionExerciseEntityByIdAndWorkoutSession(workoutSessionExerciseId, workoutSession);
@@ -132,6 +161,10 @@ public class WorkoutSessionService {
     @Transactional
     public WorkoutSessionDTO editExerciseSet(Long workoutSessionId, Long workoutSessionExerciseId, Long workoutSessionExerciseSetId, WorkoutSessionExerciseSetRequest workoutSessionExerciseSetRequest) {
         User user = userContext.getAuthenticatedUser();
+        log.info(
+                "[METHOD]: editExerciseSet - Editing workoutSessionExerciseSet by ID: {} - workoutSessionExercise by ID: {} and workoutSession by ID: {} with workoutSessionExerciseSetRequest: {}",
+                workoutSessionExerciseSetId, workoutSessionExerciseId, workoutSessionId, workoutSessionExerciseSetRequest
+        );
 
         WorkoutSession workoutSession = getWorkoutSessionEntityById(workoutSessionId, user);
         workoutSessionExerciseService.getWorkoutSessionExerciseEntityByIdAndWorkoutSession(workoutSessionExerciseId, workoutSession);
@@ -140,6 +173,7 @@ public class WorkoutSessionService {
         exerciseSet.setRepetitions(workoutSessionExerciseSetRequest.repetitions());
         exerciseSet.setWeight(workoutSessionExerciseSetRequest.weight());
 
+        log.info("[METHOD]: editExerciseSet - Saving updated workoutSessionExerciseSet by ID: {}", workoutSessionExerciseSetId);
         workoutSessionExerciseSetService.saveWorkoutSessionExerciseSet(exerciseSet);
 
         return workoutSessionMapper.toWorkoutSessionDTO(workoutSession);
