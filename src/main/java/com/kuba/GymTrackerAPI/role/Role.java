1309481456f43fb522d1de59c1c56e0cab1277d9
JakubJosefKrasa <jjkrasa@gmail.com>
2024-12-17 23:59:09 +0100

    Role package code reformatted

--- src/main/java/com/kuba/GymTrackerAPI/role/Role.java
+++ src/main/java/com/kuba/GymTrackerAPI/role/Role.java
@@ -2,25 +2,34 @@ package com.kuba.GymTrackerAPI.role;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.kuba.GymTrackerAPI.user.User;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table
 public class Role {
-    @Id@
-    GeneratedValue
+
+    @Id
+    @GeneratedValue
     private Integer id;
+
     @Column(unique = true)
     private String name;
-    @ManyToMany(mappedBy = "roles")
+
     @JsonIgnore
+    @ManyToMany(mappedBy = "roles")
     private List<User> user;
 }
--- src/main/java/com/kuba/GymTrackerAPI/role/RoleRepository.java
+++ src/main/java/com/kuba/GymTrackerAPI/role/RoleRepository.java
@@ -1,9 +1,9 @@
 package com.kuba.GymTrackerAPI.role;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
     Optional<Role> findByName(String name);
 }
